959ff83ca4b62592cf6c69e49563087bf9117b7c
ka.umanetc <ka.umanetc@stalmail.org>
2020-07-15 18:18:47 +0300

    Initial commit

---
+++ src/test/java/com/osh4/accounting/AccountingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.osh4.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
