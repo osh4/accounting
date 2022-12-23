51c9cbd5b9bce6eab55b90fef2e3711dbc1028c7
Konstantin Umanets <konstantin@osh4.com>
2022-12-23 20:15:40 +0400

    Repositories cleanup

--- src/main/java/com/osh4/accounting/persistance/repository/SettingRepository.java
+++ src/main/java/com/osh4/accounting/persistance/repository/SettingRepository.java
@@ -3,10 +3,8 @@ package com.osh4.accounting.persistance.repository;
 import com.osh4.accounting.persistance.r2dbc.Setting;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
 
 @Repository
 public interface SettingRepository extends ReactiveCrudRepository<Setting, String> {
 
-    Mono<Setting> findByKey(String key);
 }
--- src/main/java/com/osh4/accounting/persistance/repository/SettingTypeRepository.java
+++ src/main/java/com/osh4/accounting/persistance/repository/SettingTypeRepository.java
@@ -1,10 +1,10 @@
 package com.osh4.accounting.persistance.repository;
 
-import com.osh4.accounting.persistance.r2dbc.User;
+import com.osh4.accounting.persistance.r2dbc.SettingType;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends ReactiveCrudRepository<User, String> {
+public interface SettingTypeRepository extends ReactiveCrudRepository<SettingType, String> {
 
 }
