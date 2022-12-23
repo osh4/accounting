7d5f127cd4c93d65bb301357337dfd2de2022509
Konstantin Umanets <konstantin@osh4.com>
2022-12-23 10:11:54 +0400

    Add CORS configuration

---
+++ src/main/java/com/osh4/accounting/config/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.osh4.accounting.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * @author osh4 <konstantin@osh4.com>
+ */
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+    @Value("${app.cors.allowedOrigins}:''")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
