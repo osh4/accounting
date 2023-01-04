896ae4a46e90b36c00a968d137da294b70b15cc4
Konstantin Umanets <konstantin@osh4.com>
2023-01-04 15:32:29 +0400

    Add swagger

---
+++ src/main/java/com/osh4/accounting/config/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.osh4.accounting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author osh4 <konstantin@osh4.com>
+ */
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.osh4.accounting.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
