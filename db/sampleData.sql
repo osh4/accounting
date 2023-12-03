1f052e3278d617b5515b1d2076c3f4fd222076e8
osh4 <osh4@yandex.ru>
2023-12-03 22:04:22 +0400

    Fix schema, add sample data

---
+++ db/sampleData.sql
@@ -0,0 +1,5 @@
+INSERT INTO setting_types(name, class_name)
+VALUES ('test.string.setting', 'java.lang.String'),
+       ('test.int.setting', 'java.lang.Integer'),
+       ('test.long.setting', 'java.lang.Long')
+       ;
--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE accounts
     id          varchar(64) PRIMARY KEY,
     name        varchar(64),
     description varchar(64),
-    currencyId  varchar(64),
-    userId      varchar(64)
+    currency_id varchar(64),
+    user_id     varchar(64)
 );
 DROP TABLE IF EXISTS currencies;
 
@@ -35,8 +35,7 @@ DROP TABLE IF EXISTS settings;
 CREATE TABLE settings
 (
     key             varchar(200) PRIMARY KEY,
-    type            varchar(200),
-    type_java_class varchar(200),
+    setting_type_id varchar(200),
     value           varchar(200)
 );
 
@@ -44,7 +43,8 @@ DROP TABLE IF EXISTS setting_types;
 
 CREATE TABLE setting_types
 (
-    name       varchar(64) PRIMARY KEY,
+    id         varchar(64) PRIMARY KEY,
+    name       varchar(64),
     class_name varchar(200)
 );
 
@@ -52,13 +52,13 @@ DROP TABLE IF EXISTS transactions;
 
 CREATE TABLE transactions
 (
-    id                varchar(200) PRIMARY KEY,
-    transaction_date  date,
-    amount            decimal(10, 2),
-    description       varchar(200),
-    transactionTypeId varchar(200),
-    sourceAccountId   varchar(200),
-    targetAccountId   varchar(200)
+    id                  varchar(200) PRIMARY KEY,
+    transaction_date    date,
+    amount              decimal(10, 2),
+    description         varchar(200),
+    transaction_type_id varchar(200),
+    source_account_id   varchar(200),
+    target_account_id   varchar(200)
 );
 
 DROP TABLE IF EXISTS transactions_types;
@@ -74,10 +74,12 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    id       varchar(200) PRIMARY KEY,
-    email    varchar(200),
-    name     varchar(200),
-    password varchar(200),
-    roles    varchar(64)[]
+    id            varchar(200) PRIMARY KEY,
+    email         varchar(200),
+    name          varchar(200),
+    password      varchar(200),
+    roles         varchar(64)[],
+    created_date  date,
+    modified_date date
 );
 
