ad15f078f32532d03f12182ade0335163f3b5dd8
osh4 <osh4@yandex.ru>
2024-11-20 18:19:46 +0400

    Custom NotFoundException

---
+++ src/main/java/com/osh4/accounting/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.osh4.accounting.exception;
+
+/**
+ * @author osh4 <konstantin@osh4.com>
+ */
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
