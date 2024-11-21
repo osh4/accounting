62fc812fcb7b6a05524d459d71e58e6cb71b6c5d
osh4 <osh4@yandex.ru>
2024-11-21 21:27:06 +0400

    Add exceptions

---
+++ src/main/java/com/osh4/accounting/exception/AlreadyExistsException.java
@@ -0,0 +1,18 @@
+package com.osh4.accounting.exception;
+
+/**
+ * @author osh4 <konstantin@osh4.com>
+ */
+public class AlreadyExistsException extends Exception {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public static AlreadyExistsException fromCurrencyIsoCode(String isoCode) {
+        return new AlreadyExistsException(String.format("Currency with isocode [%s] already exist", isoCode));
+    }
+
+    public static AlreadyExistsException fromUserEmail(String email) {
+        return new AlreadyExistsException(String.format("User with email [%s] already exist", email));
+    }
+}
--- src/main/java/com/osh4/accounting/exception/NotFoundException.java
+++ src/main/java/com/osh4/accounting/exception/NotFoundException.java
@@ -7,4 +7,12 @@ public class NotFoundException extends Exception {
     public NotFoundException(String message) {
         super(message);
     }
+
+    public static NotFoundException fromCurrencyIsoCode(String isoCode) {
+        return new NotFoundException(String.format("Currency with isocode %s not found", isoCode));
+    }
+
+    public static NotFoundException fromUserEmail(String email) {
+        return new NotFoundException(String.format("User with email %s not found", email));
+    }
 }
